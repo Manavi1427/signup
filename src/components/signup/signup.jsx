20e41e3f283019c95d0a2b00ee2cdabbcbb769c3
Manavi1427 <manavisarkar136@gmail.com>
2024-11-24 20:07:28 +0530

    signup structure

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './signup.css'
+import usericon from '../assets/user-solid.svg'
+import passwordicon from '../assets/lock-solid.svg'
+import emailicon from '../assets/envelope-solid.svg'
 
 const signup = () => {
   return (
-    <div>
+    <div className='container'>
+        <div className="header">
+            <div className="text">
+                <p>signup</p>
+            </div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={usericon} alt="" />
+                <input type="text" placeholder='Username' />
+            </div>
+
+            <div className="input">
+                <img src={emailicon} alt="" />
+                <input type="text" placeholder='Email Address' />
+            </div>
+
+            <div className="input">
+                <img src={passwordicon} alt="" />
+                <input type="password" placeholder='Password' />
+            </div>
+        </div>
+
+        <div className="forgot_password">
+            Already have an account? 
+            <span>Sign In</span>
+        </div>
+
+        <div className="signup-container">
+            <div className="signup">
+                Sign Up
+            </div>
+        </div>
       
     </div>
   )
